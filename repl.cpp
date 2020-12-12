9f53a8c4b8a237b76d36d35e5f991dc811e6058d
joyTsing <792566033@qq.com>
2020-12-12 14:47:57 +0800

    fix bug

--- repl.cpp
+++ repl.cpp
@@ -6,7 +6,7 @@ static char buffer[2048];
 char* readline(char* prompt){
     fputs(prompt,stdout);
     fgets(buffer,2048,stdin);
-    char* cpy=malloc(strlen(buffer)+1);
+    char* cpy=(char*)malloc(strlen(buffer)+1);
     strcpy(cpy,buffer);
     cpy[strlen(cpy)-1]='\0';
     return cpy;
